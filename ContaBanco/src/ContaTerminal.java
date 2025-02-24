4ff237811ea77cc32588e122b069341792bcbaf3
FR-Sousa <filiperafael71@outlook.com>
2025-02-23 21:24:23 -0300

    add comentarios

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class ContaTerminal {
   static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
+
+        //Número da conta
         System.out.print("Digite o Número da Conta: ");
         int conta = teclado.nextInt();
 
+        //Número Agencia
         System.out.print("Digite a Agencia: ");
         String agencia = teclado.next();
 
+        //Nome do cliente
         System.out.print("Nome do Cliente: ");
         String cliente = teclado.next();
 
+        //Valor que tem bna conta
         System.out.print("Saldo da Conta: ");
         double saldo = teclado.nextDouble();
 
